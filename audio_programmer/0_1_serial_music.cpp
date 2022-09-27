185d522aa31384939ee46f90d474ea1d1686602d
lawrencedrums <lawrencedrums@gmail.com>
2022-09-27 12:57:18 +0100

    Finish CPP serial music

--- audio_programmer/0_1_serial_music.cpp
+++ audio_programmer/0_1_serial_music.cpp
@@ -1,3 +1,8 @@
+/* CPP program for analyzing serial music. P49. */
+
+#include <stdio.h>
+#include <stdlib.h>
+
 class Dodecaphonic {
     protected:
     int series[12];
@@ -15,5 +20,101 @@ class Dodecaphonic {
         }
     }
 
-    
+    Dodecaphonic(int *notes) { // constructor from an array
+	for (int i = 0; i < 12; i++) {
+	    series[i] = mod12(notes[i]);
+	}
+    }
+
+    int get(int index) { // get & set notes
+	return series[mod12(index)];
+    }
+
+    void set(int note, int index) {
+	series[mod12(index)] = mod12(note);
+    }
+
+    // three basic operations
+    Dodecaphonic transpose(int interval);
+    Dodecaphonic invert();
+    Dodecaphonic retrograde();
+};
+
+/* define the operations. All of them return a Dodecaphonic object */
+Dodecaphonic Dodecaphonic::transpose(int interval) {
+    Dodecaphonic transp;    
+
+    for (int i = 0; i < 12; i++) {
+	transp.set(mod12(series[i]+interval), i);
+    }
+
+    return transp;
 }
+
+Dodecaphonic Dodecaphonic::invert() {
+    Dodecaphonic inv;
+    inv.set(series[0], 0);
+
+    for (int i = 0; i < 12; i++) {
+	inv.set(mod12(inv.get(i-1) + series[i-1] - series[i]), i);
+    }
+
+    return inv;
+}
+
+Dodecaphonic Dodecaphonic::retrograde() {
+    Dodecaphonic retr;
+    
+    for (int i = 0; i < 12; i++) {
+	retr.set(series[i], 11-i);
+    }
+
+    return retr;
+}
+
+int main(int argc, char** argv) {
+    Dodecaphonic row, res;
+    int interval, n;
+
+	if (argc != 14 || argv[1][0] != '-') {
+		printf("Usage: %s [-oN | -rN | -iN | -irN ] "
+               "note1 note2 ... note12\n",
+               argv[0]);
+		return -1; 
+	}
+	
+	for (n = 0; n < 12; n++) { /* init row object */
+		row.set(atoi(argv[n+2]), n);
+	}
+
+	switch(argv[1][1]) {
+	case 'o': /* original transpose */
+		interval = atoi(argv[1]+2);
+		res = row.transpose(interval);
+		break;
+	case 'r': /* retrograde */
+		interval = atoi(argv[1]+2);
+		res = row.retrograde().transpose(interval);
+		break;
+	case 'i': /* inverted */
+		if (argv[1][3] != 'r') {
+			interval = atoi(argv[1]+2);
+			res = row.invert().transpose(interval);
+		} else { /* inverted retrograde */
+			interval = atoi(argv[1]+3);
+			res = row.invert().retrograde().transpose(interval);
+		}
+		break;
+	default:
+		printf("unrecognized option \n");
+		return -1;
+	}
+	
+	for (n = 0; n < 12; n++) {
+		printf("%d ", res.get(n));
+	}
+	printf("\n");
+	
+    return 0;
+}
+
