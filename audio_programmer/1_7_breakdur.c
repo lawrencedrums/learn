00152f77231e19167ee605b5ef9ac0ab4ce610c3
Lawrence Wong <lawrencedrums@gmail.com>
2022-10-31 18:44:07 +0000

    Finished Listing 1.7.4 breakdur

---
+++ audio_programmer/1_7_breakdur.c
@@ -0,0 +1,140 @@
+/* Listing 1.7.4. P.145. */
+
+/* Validate a text file of breakpoints
+ * Prints the duration and max value of the breakpoints  
+ */
+
+#include<stdio.h>
+#include<stdlib.h>
+
+
+typedef struct breakpoint {
+    double time;
+    double value;
+} BREAKPOINT;
+
+
+/* input: points = array of BREAKPOINTS,
+ *        npoints gives the length of the array 
+ * output: copy of the BREAKPOINT containing largest value 
+ */
+BREAKPOINT maxpoint(const BREAKPOINT* points, long npoints) {
+    BREAKPOINT maxpoint;
+
+    maxpoint.time = points[0].time; /* initialize from first point */
+    maxpoint.value = points[0].value;
+    
+    for (int i = 1; i < npoints; i++) {
+        if (maxpoint.value < points[i].value) {
+            maxpoint.value = points[i].value;
+            maxpoint.time = points[i].time;
+        }
+    }
+    return maxpoint;
+}
+
+
+BREAKPOINT* get_breakpoints(FILE* fp, long* psize) {
+    int got;
+    long npoints = 0, size = 64;
+    double lasttime = 0.0;
+    BREAKPOINT* points = NULL;
+    char line[80];
+
+    if (fp == NULL) return NULL;
+
+    points = (BREAKPOINT*) malloc(sizeof(BREAKPOINT) * size);
+    if (points == NULL) return NULL;
+
+    while (fgets(line, 80, fp)) {
+        got = sscanf(line, "%lf %lf",
+                &points[npoints].time, &points[npoints].value);
+
+        if (got < 0) continue; /* empty line */
+        if (got == 0) {
+            printf("Line %d has non-numeric data\n", npoints+1);
+            break;
+        }
+        if (got == 1) {
+            printf("Incomplete breakpoint found at point %d\n",
+                    npoints+1);
+            break;
+        }
+    
+        lasttime = points[npoints].time;
+        printf("%f %f\n", points[npoints].time, points[npoints].value);
+    
+        if (++npoints == size) {
+            BREAKPOINT* tmp;
+            size += npoints;
+            tmp = (BREAKPOINT*) realloc(points, sizeof(BREAKPOINT) * size);
+
+            if (tmp == NULL) {
+                /* have to release the memory, and return NULL */
+                npoints = 0;
+                free(points);
+                points = NULL;
+                break;
+            }
+            points = tmp;
+        }
+    }
+
+    if (npoints) *psize = npoints;
+    return points;
+}
+
+
+int main(int argc, char* argv[]) {
+	long size;
+    double dur;
+    BREAKPOINT point, *points;
+    FILE* fp;
+
+    printf("breakdur: find duration of breakpoint file\n");
+    
+    if (argc < 2) {
+        printf("usage: breakdur infile.txt\n");
+        return 0;
+    }
+
+    fp = fopen(argv[1], "r");
+    if (fp == NULL) return 0;
+
+    size = 0;
+    points = get_breakpoints(fp, &size);
+
+    if (points == NULL) {
+        printf("No breakpoints read.\n");
+        fclose(fp);
+        return 1;
+    }
+    if (size < 2) {
+        printf("Error: at least two breakpoints required\n");
+        free(points);
+        fclose(fp);
+        return 1;
+    }
+
+    /* we require breakpoints to start from 0 */
+    if (points[0].time != 0.0) {
+        printf("Error in breakpoint data: first time must be 0.0\n");
+        free(points);
+        fclose(fp);
+        return 1;
+    }
+ 
+    printf("read %d breakpoints\n", size);
+
+    dur = points[size-1].time;
+    printf("duration: %f seconds\n", dur);
+    
+    point = maxpoint(points, size);
+    printf("maximum value: %f at %f secs\n", point.value, point.time);
+    
+    free(points);
+    fclose(fp);
+    
+    return 0;
+}
+
