0574f1f79ba9bb47d7544df83635abb2f9b5eefe
Lawrence Wong <lawrence.wmc@pm.me>
2023-02-26 23:56:14 +0000

    if let control flow

---
+++ rust/6_3_if_let/src/main.rs
@@ -0,0 +1,35 @@
+enum Lang {
+    Python,
+    Rust,
+}
+
+fn main() {
+    // if let is a less verbose way to handles values
+    // that matches one pattern and ignore the rest
+    let config_max = Some(3u8);
+    if let Some(max) = config_max {
+        println!("The maximum is configured to be {}", max);
+    }
+    // the above is the same as...
+    // match config_max {
+    //     Some(max) => println!("The maximum is configured to be {}", max),
+    //     _ => (), // <- this line is annoying boilerplate!
+    // }
+    
+    let mut count = 0;
+    let lang = Lang::Rust;
+    if let Lang::Rust = lang {
+        println!("One of us! One of us!");
+        count += 1;
+    } else {
+        println!("Python... typical");
+    }
+    // the above is the same as...
+    // match lang {
+    //     Lang::Rust => {
+    //         println!("One of us! One of us!");
+    //         count += 1;
+    //     }
+    //     _ => println!("Python... typical");
+    // }
+}
