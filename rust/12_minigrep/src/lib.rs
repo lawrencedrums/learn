d4cbefc7f8fb4fb779da519f11b3c2e8c1372705
Lawrence Wong <lawrence.wmc@pm.me>
2023-05-19 14:31:18 +0100

    12-minigrep: standard error

--- rust/12_minigrep/src/lib.rs
+++ rust/12_minigrep/src/lib.rs
@@ -1,9 +1,11 @@
+use std::env;
 use std::error::Error;
 use std::fs;
 
 pub struct Config {
-    query: String,
-    file_path: String,
+    pub query: String,
+    pub file_path: String,
+    pub ignore_case: bool,
 }
 
 impl Config {
@@ -15,14 +17,26 @@ impl Config {
         let query = args[1].clone();
         let file_path = args[2].clone();
 
-        Ok(Config { query, file_path })
+        let ignore_case = env::var("IGNORE_CASE").is_ok();
+
+        Ok(Config {
+            query,
+            file_path,
+            ignore_case,
+        })
     }
 }
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.file_path)?;
 
-    for line in search(&config.query, &contents) {
+    let results = if config.ignore_case {
+        search_case_insensitive(&config.query, &contents)
+    } else {
+        search(&config.query, &contents)
+    };
+
+    for line in results {
         println!("{line}");
     }
 
@@ -41,6 +55,22 @@ pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     results
 }
 
+pub fn search_case_insensitive<'a>(
+    query: &str,
+    contents: &'a str
+) -> Vec<&'a str> {
+    let query = query.to_lowercase();
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.to_lowercase().contains(&query) {
+            results.push(line);
+        }
+    }
+
+    results
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -49,13 +79,27 @@ mod tests {
     fn test_search_one_result() {
         let query = "wings";
         let contents = "\
-            Just you imagine the damage that's done
-            when you fly with wax wings in the sun.
-            The machine is turning me";
+Just you imagine the damage that's done
+when you fly with wax wings in the sun.
+The machine is turning me";
 
         assert_eq!(
             vec!["when you fly with wax wings in the sun."],
             search(query, contents)
+        );
+    }
+
+    #[test]
+    fn test_case_insensitive_search() {
+        let query = "rogue";
+        let contents = "\
+The Rogue is an adaptable, agile warrior
+who can specialize in ranged
+or close quarters combat.";
+
+        assert_eq!(
+            vec!["The Rogue is an adaptable, agile warrior"],
+            search_case_insensitive(query, contents)
         );
     }
 }
